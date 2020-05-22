208faef29c9754bb69762823b6f0d90c3f78eb49
Some-Old-Git <65522904+Some-Old-Git@users.noreply.github.com>
2020-05-22 20:20:02 +1000

    Update Calculator .py
    Exception handling

--- Calculator .py
+++ Calculator .py
@@ -5,46 +5,108 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def error(x):
+    # Handle input and computation errors by type
+    print("An error has occurred")
+    if x == 'zero':
+        print("Unable to divide by zero")
+        cls()
+        menu()
+    elif x == 'over':
+        print("Number too large")
+        cls()
+        menu()
+    elif x == 'val' or 'typ':
+        print("Please try again")
+        cls()
+        menu()
+    else:
+        cls()
+        menu()
+
+
 def add(x, y):
-    return float(x) + float(y)
+    # Return the sum of the two inputs
+    # Output any relevant error by type
+    try:
+        return float(x) + float(y)
+    except ValueError:
+        error('val')
+    except OverflowError:
+        error('over')
+    except TypeError:
+        error('typ')
 
 
 def subtract(x, y):
-    return float(x) - float(y)
+    # Return the difference between the two inputs
+    # Output any relevant error by type
+    try:
+        return float(x)-float(y)
+    except ValueError:
+        error('val')
+    except OverflowError:
+        error('over')
+    except TypeError:
+        error('typ')
 
 
 def multiply(x, y):
-    return float(x) * float(y)
+    # Return the product of the two inputs
+    # Output any relevant error by type
+    try:
+        return float(x) * float(y)
+    except ValueError:
+        error('val')
+    except OverflowError:
+        error('over')
+    except TypeError:
+        error('typ')
 
 
 def divide(x, y):
-    return float(x) / float(y)
+    # Return the quotient of the two inputs
+    # Output any relevant error by type
+    try:
+        return float(x) / float(y)
+    except ZeroDivisionError:
+        error('zero')
+    except ValueError:
+        error('val')
+    except OverflowError:
+        error('over')
+    except TypeError:
+        error('typ')
 
 
 def menu():
     while True:
+        # Obtain user operation selection
         print("Which operation do you wish to perform?")
-        print("Äddition + '?' \nSubtraction '-' ? \nMultiplication '*' ? \nor Division '/' ?")
+        print("1. Addition '+'? \n2. Subtraction '-'? \n3. Multiplication '*'? \n4. Division '/'?")
         print("Enter 'quit' to quit the program")
-        user_input = input(": ")
-        if user_input == "quit":
+        user_input = input(":  ")
+        if user_input == 'quit':
+            # Quit program
             cls()
-            exit()
+            break
         else:
+            # Obtain user input to then perform desired operation and output
             x = float(input("First number?:"))
             y = float(input("Second number?:"))
-            if user_input == "add" or "+":
-                print(add(x, y))
-            elif user_input == "subtract" or "-":
+            if user_input == 'addition' or '+' or 'add' or '1':
+                print(add(x,y))
+            elif user_input == 'subtraction' or '-' or 'subtract' or '2':
                 print(subtract(x, y))
-            elif user_input == "multiply" or "*":
+                continue
+            elif user_input == 'multiplication' or '*' or 'multiply' or '3' or 'x':
                 print(multiply(x, y))
-            elif user_input == "divide" or "/":
+                continue
+            elif user_input == 'division' or '/' or 'divide' or '4':
                 print(divide(x, y))
+                continue
             else:
-                print("Unknown input")
-                cls()
-                menu()
+                error('val')
 
 
 menu()
